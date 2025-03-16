2bfaadd16c5b4fd151f62a002426a140430f00aa
znguye <dnguyen.jmblc@gmail.com>
2025-03-16 17:48:21 +0100

    done

---
+++ src/components/Message.jsx
@@ -0,0 +1,7 @@
+export default function Message({message}) {
+  return (
+    <p className="message">
+      {message}
+    </p>
+  );
+}
---
+++ src/components/Timestamp.jsx
@@ -0,0 +1,6 @@
+export default function Timestamp({time}) {
+  return (
+    <span className="timestamp"> {time} </span>
+  );
+}
+
